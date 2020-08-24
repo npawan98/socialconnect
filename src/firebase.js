d2913b7ae4752e2e30e8b84c71454bda636a9b1a
Pawan Kumar <44532194+npawan98@users.noreply.github.com>
2020-08-24 18:56:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDTIDFnOWPzxICnS7pv4XjDXw7MPYZ8YWc",
-    authDomain: "socialconnect-2df6b.firebaseapp.com",
-    databaseURL: "https://socialconnect-2df6b.firebaseio.com",
-    projectId: "socialconnect-2df6b",
-    storageBucket: "socialconnect-2df6b.appspot.com",
-    messagingSenderId: "182442502825",
-    appId: "1:182442502825:web:e5a31b39f914fd520e97b8",
-    measurementId: "G-XJKT6NL0HE"
+   //api goes here 
 
 });
 
